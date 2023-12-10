c2fd804b10da8476ecabe598f7ea6efddc1a5c9e
Jamsjz <gj984233@gmail.com>
2023-12-10 07:21:49 +0545

    add tictactoe game

---
+++ tictactoe/tictactoe.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+char board[3][3];
+void initializeBoard() {
+  int i, j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      board[i][j] = ' ';
+    }
+  }
+  int count = 1;
+  printf("\n\n\t ");
+
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%d", count++);
+      if (j < 2) {
+        printf("  |  ");
+      }
+    }
+    if (i < 2)
+      printf("\n\t----------------\n\t ");
+  }
+  printf("\n\n\n");
+}
+void showBoard(int x, int y) {
+  printf("\n\n\t ");
+  int i, j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%c", board[i][j]);
+      if (j < 2) {
+        printf("  |  ");
+      }
+    }
+    if (i < 2)
+      printf("\n\t----------------\n\t");
+  }
+  printf("\n\n\n");
+}
+int updateBoard(int cell, char playerSign) {
+  int row = (cell - 1) / 3;
+  int col = (cell - 1) % 3;
+  int isValid = 1;
+
+  if (board[row][col] != ' ') {
+    printf("\n Invalid: Cell is already Filled!\n");
+    isValid = 0;
+  } else {
+    board[row][col] = playerSign;
+  }
+  showBoard(row, col);
+  return isValid;
+}
+int checkWinner(char sg) {
+  if (board[0][0] == sg && board[0][1] == sg && board[0][2] == sg ||
+      board[1][0] == sg && board[1][1] == sg && board[1][2] == sg ||
+      board[2][0] == sg && board[2][1] == sg && board[2][2] == sg) {
+    return 1;
+  } else if (board[0][0] == sg && board[1][0] == sg && board[2][0] == sg ||
+             board[0][1] == sg && board[1][1] == sg && board[2][1] == sg ||
+             board[0][2] == sg && board[1][2] == sg && board[2][2] == sg) {
+    return 1;
+  } else if (board[0][0] == sg && board[1][1] == sg && board[2][2] == sg ||
+             board[0][2] == sg && board[1][1] == sg && board[2][0] == sg) {
+    return 1;
+  }
+  return 0;
+}
+void playTicTacToe() {
+  int gameResult = 0;
+  int cell = 0;
+  int playCount = 0;
+  int updationResult = 1;
+  char playerSign = ' ';
+  while (!gameResult && playCount < 9) {
+    if (playCount % 2 == 0) {
+      printf("\n Player 1 [X]:");
+      playerSign = 'X';
+    } else {
+      printf("\n Player 2 [O]:");
+      playerSign = 'O';
+    }
+    scanf("%d", &cell);
+    if (cell > 0 && cell < 10) {
+      updationResult = updateBoard(cell, playerSign);
+      if (updationResult) {
+        gameResult = checkWinner(playerSign);
+        if (gameResult) {
+          printf("\t ***Player %d WOn!!***\n", playerSign == 'X' ? 1 : 2);
+        }
+        playCount++;
+      }
+    } else if (cell == -1) {
+      printf("\n\tGame Terminated\n");
+      return;
+    } else {
+      printf("\nPlease Enter a Valid cell value\n");
+    }
+  }
+  if (!gameResult && playCount == 9) {
+    printf("\n\t*** Draw... ***\n");
+  }
+  printf("\n\t --- Game Over --- \n");
+}
+int main() {
+  printf("------Tic Tac Toe------\n\n");
+  printf("\n * Instruction \n\n");
+  printf("\tPlayer 1 sign=X\n");
+  printf("\tPlayer 2 sign=O");
+  printf("\n\t To exit from game,Enter -1\n");
+  printf("\n\n* Cell Number on Board\n");
+  initializeBoard();
+  char start;
+  start = ' ';
+  printf(">>>>> Press Enter to start...");
+  scanf("%c", &start);
+  if (start) {
+    int userChoice = 1;
+    while (userChoice) {
+      playTicTacToe();
+      printf("\n* Menu\n");
+      printf("\nPress 1 To Restart");
+      printf("\nPress 0 for exit");
+      printf("\n\nChoice:");
+      scanf("%d", &userChoice);
+      if (userChoice) {
+        initializeBoard();
+      }
+      printf("\n");
+    }
+  }
+  printf("\n :: Thanks for playing Tic Tac Toe game! :: \n");
+  return 0;
+}
